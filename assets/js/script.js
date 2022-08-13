d2997cfb91ef89a51a27be497de51e0632e6813f
NickWaldock <nickwaldock@me.com>
2022-08-13 07:08:57 +0000

    add js core functions

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+function runGame(){
+
+
+}
+
+
+function checkAnswer(){
+
+
+}
+
+function calculateCorrectAnswer(){
+
+
+}
+
+function incrementScore(){
+
+
+}
+
+function incrementWrongAnswer() {
+
+
+}
+
+function displayAdditionQuestion(){
+
+
+}
+
+function displaySubtractQuestion(){
+
+
+}
+
+function displayMulitplyQuestion(){
+
+
+}
+
